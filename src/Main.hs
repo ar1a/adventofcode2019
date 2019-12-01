c5750a9560343b563d39e729f1a3f40d3a56c3cd
Aria Edmonds <aria@ar1as.space>
2019-12-01 17:45:34 +1100

    day 1 part 2

--- src/Main.hs
+++ src/Main.hs
@@ -7,5 +7,11 @@ main = do
   let numbers = read <$> input'
   print $ sum $ calculate <$> numbers
 
-calculate :: Float -> Int
-calculate mass = floor (mass / 3) - 2
+calculate :: Integer -> Integer
+calculate mass = go $ fuelcalc mass
+ where
+  fuelcalc :: Integer -> Integer
+  fuelcalc x = floor (fromInteger x / 3) - 2
+  go :: Integer -> Integer
+  go x | fuelcalc x > 0 = x + go (fuelcalc x)
+       | otherwise      = x
